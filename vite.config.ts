d9384bfe9610c9590a328242ff55763feef3ca87
Aman Kumar Gupta <gupta.aman7079@gmail.com>
2026-06-15 07:00:09 +0530

    get free not working correct

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  publicDir: "public",
   build: {
     outDir: "dist",
+    emptyOutDir: true,
   },
 });
